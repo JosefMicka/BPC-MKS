8528d4570da5eacad6a71299e1b55ed331672381
JosefMicka <jozkamic@seznam.cz>
2020-09-30 23:23:20 +0200

    cv01 - Blikani SOS

--- cv01_F030/Src/main.c
+++ cv01_F030/Src/main.c
@@ -26,11 +26,26 @@
 
 int main(void)
 {
+	static const uint8_t morse[32] = {1, 0, 1, 0, 1, 0, 0, 1,
+										1, 1, 0, 1, 1, 1, 0, 1,
+										1, 1, 0, 0, 1, 0, 1, 0,
+										1, 0, 0, 0, 0, 0, 0, 0 };
+
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	GPIOA->MODER |= GPIO_MODER_MODER5_0;
 
 	while(1){
-		GPIOA->ODR ^= (1<<5);
-		for(volatile uint32_t i = 0; i < 100000; i++){}
+
+		for(uint32_t index = 0; index < sizeof(morse); index++){
+
+			if(morse[index]){
+				GPIOA->BSRR = (1<<5); // set
+			}
+			else{
+				GPIOA->BRR = (1<<5); // reset
+			}
+
+			for(volatile uint32_t i = 0; i < 100000; i++);
+		}
 	}
 }
