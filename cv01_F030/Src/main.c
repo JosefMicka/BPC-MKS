e273d60b3f782a0efa51ad45c3484648f2c010f7
JosefMicka <jozkamic@seznam.cz>
2020-09-30 23:06:48 +0200

    cv01 - blikani LED

--- cv01_F030/Src/main.c
+++ cv01_F030/Src/main.c
@@ -17,12 +17,20 @@
  ******************************************************************************
  */
 
+#include "stm32f0xx.h"
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
+	while(1){
+		GPIOA->ODR ^= (1<<5);
+		for(volatile uint32_t i = 0; i < 100000; i++){}
+	}
 }
