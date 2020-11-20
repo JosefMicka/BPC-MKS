8254281e7d394b19e70fd92ea914f63b2229ad7d
JosefMicka <jozkamic@seznam.cz>
2020-11-20 11:33:11 +0100

    cv06_F401_TEMP + cv07_F030_RTOS, cv07_F401_RTOS - nedokončeno, kvůli hardfaultu

---
+++ cv06_F401_TEMP/Core/Inc/sct.h
@@ -0,0 +1,18 @@
+/*
+ * sct.h
+ *
+ *  Created on: 15. 10. 2020
+ *      Author: Josef Micka
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_led(uint32_t value);
+
+void sct_init(void);
+
+//void sct_value(uint16_t value);
+void sct_value(uint16_t value, uint8_t decpoint);
+
+#endif /* SCT_H_ */
